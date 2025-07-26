3c086feddf6c9c3ab1cdcfdb30f3edb373910b8c
Maxim Antonov <maxd.antonov@gmail.com>
2025-07-26 14:16:22 +0300

    Отслеживание потенциальной и кинетической энергии

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 class Particle():
-    def __init__(self, pclsys, ep, sigma, m, cord):
+    def __init__(self, pclsys, eps, sigma, m, cord):
         self.cord = cord # в СИ, м
         self.vel = np.array([0.0, 0.0]) # в СИ, м/с
         self.pclsys = pclsys
         self.sigma = sigma
-        self.ep = ep
+        self.eps = eps
         self.m = m
 
     def calc_acceleration(self):
         # params: pclsys, vel, cord
-        ep = self.ep
+        ep = self.eps
         sigma = self.sigma
         F = np.array([0.0, 0.0])
         L = self.pclsys.L  # Размер квадрата
@@ -46,6 +46,34 @@ class Particle():
         self.cord[1] = self.cord[1] % L
         
         self.vel = self.vel + 1/2 * (acc + self.calc_acceleration()) * dt
+    
+    def calc_Ek(self):
+        return 0.5 * self.m * np.sum(self.vel**2)
+    
+    def calc_Ep(self):
+        def V(r):
+            return 4*self.eps*((self.sigma/r)**12 - (self.sigma/r)**6)
+        
+        ep = 0.0
+        L = self.pclsys.L
+        
+        for p in self.pclsys.particles:
+            if p is self:
+                continue
+                
+            # Рассчитываем расстояние с учетом периодических граничных условий
+            r = self.cord - p.cord
+            
+            # Применяем периодические граничные условия для расчета кратчайшего расстояния
+            r[0] = r[0] - L * round(r[0] / L)
+            r[1] = r[1] - L * round(r[1] / L)
+            
+            r_abs = np.sqrt(np.sum(r**2))
+            if r_abs == 0.0:
+                continue
+            ep += V(r_abs)
+        return ep
+    
 
 class ParticleSystem():
     def __init__(self, N, ep, sigma, m, L):
@@ -76,10 +104,20 @@ class ParticleSystem():
         for particle in self.particles:
             particle.update(dt)
         self.time += dt
+    
+    def calc_Ek(self):
+        return sum(p.calc_Ek() for p in self.particles)
+    
+    def calc_Ep(self):
+        return sum(p.calc_Ep() for p in self.particles) / 2
+    
+    def calc_E(self):
+        return self.calc_Ek() + self.calc_Ep()
 
 # Пусть наш газ - Аргон
 EPSILON = 1.66e-21 # Дж
 SIGMA = 3.41e-10 # м
+MASS = 6.63e-26 # кг
 
 def main():
     pass
